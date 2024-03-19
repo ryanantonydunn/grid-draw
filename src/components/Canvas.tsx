360894067a45be2b02ed68785030d6f7cd5ae7a4
Ryan Dunn <contact@ryandunn.dev>
2024-03-19 14:39:56 +0000

    helper numbers

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -26,49 +26,76 @@ export function Canvas() {
 
   const { lines, activePosition } = image.state.present;
 
-  return (
-    <div
-      className="h-screen"
-      style={{
-        backgroundColor: canvasOptions.gridBg,
-        backgroundImage: `linear-gradient(to right,${canvasOptions.gridFg} 0,${canvasOptions.gridFg} 1px,transparent 1px,transparent 100%),linear-gradient(to bottom,${canvasOptions.gridFg} 0,${canvasOptions.gridFg} 1px,transparent 1px,transparent 100%)`,
-        backgroundSize: `${canvasOptions.gridSize}px ${canvasOptions.gridSize}px`,
-        backgroundPosition: "-1px -1px",
-      }}
-      onMouseMove={(e) => {
-        setHoverPosition(getPositionFromXy(getXyFromEvent(e)));
-      }}
-      onMouseLeave={() => {
-        setHoverPosition(null);
-      }}
-      onMouseDown={(e) => {
-        if (e.button === 2) {
-          clearActivePosition();
-        } else {
-          clickPosition(getPositionFromXy(getXyFromEvent(e)));
+  const offset =
+    hoverPosition && activePosition
+      ? {
+          x: hoverPosition[0] - activePosition[0],
+          y: hoverPosition[1] - activePosition[1],
         }
-      }}
-      onContextMenu={(e) => {
-        e.preventDefault();
-      }}
-    >
-      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
-        {lines.map((line, i) => (
-          <DrawLine key={i} {...line} />
-        ))}
-        <DrawLine start={activePosition} end={hoverPosition} {...lineOptions} />
-        <DrawCircle
-          position={activePosition}
-          color={canvasOptions.selectedCircleColor}
-          size={canvasOptions.selectedCircleSize}
-        />
-        <DrawCircle
-          position={hoverPosition}
-          color={canvasOptions.hoverCircleColor}
-          size={canvasOptions.hoverCircleSize}
-        />
-      </svg>
-    </div>
+      : null;
+
+  return (
+    <>
+      <div
+        className="h-screen"
+        style={{
+          backgroundColor: canvasOptions.gridBg,
+          backgroundImage: `linear-gradient(to right,${canvasOptions.gridFg} 0,${canvasOptions.gridFg} 1px,transparent 1px,transparent 100%),linear-gradient(to bottom,${canvasOptions.gridFg} 0,${canvasOptions.gridFg} 1px,transparent 1px,transparent 100%)`,
+          backgroundSize: `${canvasOptions.gridSize}px ${canvasOptions.gridSize}px`,
+          backgroundPosition: "-1px -1px",
+        }}
+        onMouseMove={(e) => {
+          setHoverPosition(getPositionFromXy(getXyFromEvent(e)));
+        }}
+        onMouseLeave={() => {
+          setHoverPosition(null);
+        }}
+        onMouseDown={(e) => {
+          if (e.button === 2) {
+            clearActivePosition();
+          } else {
+            clickPosition(getPositionFromXy(getXyFromEvent(e)));
+          }
+        }}
+        onContextMenu={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+          {lines.map((line, i) => (
+            <DrawLine key={i} {...line} />
+          ))}
+          <DrawLine start={activePosition} end={hoverPosition} {...lineOptions} />
+          <DrawCircle
+            position={activePosition}
+            color={canvasOptions.selectedCircleColor}
+            size={canvasOptions.selectedCircleSize}
+          />
+          <DrawCircle
+            position={hoverPosition}
+            color={canvasOptions.hoverCircleColor}
+            size={canvasOptions.hoverCircleSize}
+          />
+        </svg>
+      </div>
+      <div className="flex absolute top-0 left-0 text-xs text-white bg-slate-900 bg-opacity-70">
+        <div className="w-10 p-2">{hoverPosition?.[0] || "0"}</div>
+        <div className="w-10 p-2">{hoverPosition?.[1] || "0"}</div>
+        {offset && (
+          <>
+            <div className="w-px self-stretch bg-white bg-opacity-30"></div>
+            <div className="w-11 p-2">
+              {offset.x > 0 && "+"}
+              {offset.x}
+            </div>
+            <div className="w-11 p-2">
+              {offset.y > 0 && "+"}
+              {offset.y}
+            </div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
